6a035ab9da553fdb8ac60260b3e7c6753ae18992
lunksana <zoufeng4@gmail.com>
2018-01-17 12:30:53 +0800

    session

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 filepath='/aria2/conf/aria2.conf'
-essionpath='/aria2/conf/aria2.ession'
+sessionpath='/aria2/conf/aria2.session'
 if [ ! -f $filepath ]; then
 cp /aria2/aria2.conf /aria2/conf
 echo "Please modify the configuration file!"
-elif [ ! -f $essionpath ]; then
-touch /aria2/conf/aria2.ession
+elif [ ! -f $sessionpath ]; then
+touch /aria2/conf/aria2.session
 else
 /usr/bin/aria2c --conf-path=/aria2/conf/aria2.conf
 fi

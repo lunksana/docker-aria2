40104484b567bc434bc9275327f289f2c61f4ccf
lunksana <zoufeng4@gmail.com>
2018-01-17 15:44:00 +0800

    dir

--- start.sh
+++ start.sh
@@ -7,5 +7,5 @@ echo "Please modify the configuration file!"
 elif [ ! -f $sessionpath ]; then
 touch /aria2/conf/aria2.session
 else
-/usr/bin/aria2c --conf-path=/aria2/conf/aria2.conf
+/usr/bin/aria2c --conf-path=/aria2/conf/aria2.conf --dir=/aria2/downloads
 fi

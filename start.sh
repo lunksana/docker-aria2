7bfcdbf4a7f9b4376c81040c66c51e5b59c8ce44
lunksana <zoufeng4@gmail.com>
2018-01-18 10:00:03 +0800

    add dht

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ if [ ! -f $filepath ]; then
 cp /aria2/aria2.conf /aria2/conf
 echo "Please modify the configuration file!"
 elif [ ! -f $sessionpath ]; then
-touch /aria2/conf/aria2.session
+touch /aria2/conf/aria2.session && \
 cp /aria2/dht.dat /aria2/conf
 else
 /usr/bin/aria2c --conf-path=/aria2/conf/aria2.conf --dir=/aria2/downloads \

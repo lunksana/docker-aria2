d2116062db0df87e5868f65fb893cbfd06d5911d
lunksana <zoufeng4@gmail.com>
2018-01-18 13:50:52 +0800

    v0.1.7

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # Docker-aria2
 
 ## 1.版本
-当前版本为最新版
+Aria2版本：1.33.1
 
 ## 2.说明
+自制Aria2镜像，添加了DHT网络支持，并开启了rpc支持，默认密钥为password
+
+## 3.使用方法
+抓取容器：docker pull lunksana/aria2-docker
+挂载容器/aria2/conf和/aria2/downloads至宿主机任意目录，并保证该目录具有写入权限
+映射容器端口6800至宿主机任意端口
+
+> 运行命令
+docker run --name myaria2 -v /path/conf:/aria2/conf -v /path/downloads:/aria2/downloads -p 6800:6800 -d lunksana/aria2-docker:latest
+
+> 配置文件
+容器会自动生成aria2.conf配置文件，可根据自身需要对该文件进行调整
--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 filepath='/aria2/conf/aria2.conf'
 sessionpath='/aria2/conf/aria2.session'
+dhtpath='/aria2/conf/dht.dat'
 if [ ! -f $filepath ]; then
 cp /aria2/aria2.conf /aria2/conf
 echo "Please modify the configuration file!"
 elif [ ! -f $sessionpath ]; then
-touch /aria2/conf/aria2.session && \
+touch /aria2/conf/aria2.session
+elif [ ! -f $dhtpath ]; then
 cp /aria2/dht.dat /aria2/conf
 else
 /usr/bin/aria2c --conf-path=/aria2/conf/aria2.conf --dir=/aria2/downloads \

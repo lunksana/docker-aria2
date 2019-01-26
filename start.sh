f50b2ad840f6c24708b0a420996355e488da8fff
lunksana <zoufeng4@gmail.com>
2019-01-26 13:31:32 +0800

    拼写错误

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ dhtpath='/aria2/conf/dht.dat'
 list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
 
 cp /aria2/aria2.conf /aria2/conf
-touch /aria2/conf/aria2.senssion
+touch /aria2/conf/aria2.session
 cp /aria2/dht.dat /aria2/conf
 sed -i "s@bt-tracker*@bt-tracker=$list@g" $filepath
 /usr/bin/aria2c --conf-path=$filepath --dir=/aria2/downloads \

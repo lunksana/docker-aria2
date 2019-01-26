cb886186855746d5bc188ab39d6b110b4d723ebb
lunksana <zoufeng4@gmail.com>
2019-01-26 13:30:44 +0800

    修复文件复制错误

--- start.sh
+++ start.sh
@@ -5,8 +5,8 @@ dhtpath='/aria2/conf/dht.dat'
 list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
 
 cp /aria2/aria2.conf /aria2/conf
-cp /aria2/aria2.senssion /aria2/conf
-cp /aria2/dht.dat /aria2.conf
+touch /aria2/conf/aria2.senssion
+cp /aria2/dht.dat /aria2/conf
 sed -i "s@bt-tracker*@bt-tracker=$list@g" $filepath
 /usr/bin/aria2c --conf-path=$filepath --dir=/aria2/downloads \
 --rpc-secret=${SECRET}

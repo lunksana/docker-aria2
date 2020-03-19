70058dab69ed57376dff2fc3b8d6cb43ee330726
lunksana <zoufeng4@gmail.com>
2020-03-19 10:32:33 +0800

    2020 test

--- start.sh
+++ start.sh
@@ -2,13 +2,13 @@
 filepath='/aria2/conf/aria2.conf'
 sessionpath='/aria2/conf/aria2.session'
 dhtpath='/aria2/conf/dht.dat'
-list="wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt|awk NF|sed ":a;N;s/\n/,/g;ta""
+list=$(wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt|awk NF|sed ":a;N;s/\n/,/g;ta")
 
 if [ ! -f "$filepath" ]; then
     cp /aria2/aria2.conf /aria2/conf
     touch /aria2/conf/aria2.session
     cp /aria2/dht.dat /aria2/conf
 fi
-sed -i "s@bt-tracker*@bt-tracker=$list@g" $filepath
+sed -i "s/^bt-tracker.*$/bt-tracker=$list/g" $filepath
 /usr/bin/aria2c --conf-path=$filepath --dir=/aria2/downloads \
 --rpc-secret=${SECRET}
